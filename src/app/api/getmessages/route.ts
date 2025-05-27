289ab7fb5bae627e6f5bf86ddb0ea6153d12df79
Rupam Das <rupam1624@gmail.com>
2025-05-28 03:39:05 +0530

    Update route.ts

--- src/app/api/getmessages/route.ts
+++ src/app/api/getmessages/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/options";
 import dbConnect from "@/lib/dbConnect";
@@ -5,7 +6,8 @@ import UserModel from "@/model/User";
 import { User } from "next-auth";
 import mongoose from "mongoose";
 
-export async function GET(_request: Request) {
+export async function GET(request: Request) {
+
   await dbConnect();
   const session = await getServerSession(authOptions);
   const user: User = session?.user as User;
