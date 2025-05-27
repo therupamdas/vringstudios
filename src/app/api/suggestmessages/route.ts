9e2d94f889fd047d4ae36a5199c19abdeb6d6f48
Rupam Das <rupam1624@gmail.com>
2025-05-27 22:55:47 +0530

    Update route.ts

--- src/app/api/suggestmessages/route.ts
+++ src/app/api/suggestmessages/route.ts
@@ -1,16 +1,16 @@
-import { openai } from '@ai-sdk/openai';
-import { streamText } from 'ai';
+// import { openai } from '@ai-sdk/openai';
+// import { streamText } from 'ai';
 
-// Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
+// // Allow streaming responses up to 30 seconds
+// export const maxDuration = 30;
 
-export async function POST(req: Request) {
-  const { messages } = await req.json();
+// export async function POST(req: Request) {
+//   const { messages } = await req.json();
 
-  const result = streamText({
-    model: openai('gpt-4o'),
-    messages,
-  });
+//   const result = streamText({
+//     model: openai('gpt-4o'),
+//     messages,
+//   });
 
-  return result.toDataStreamResponse();
-}
+//   return result.toDataStreamResponse();
+// }
