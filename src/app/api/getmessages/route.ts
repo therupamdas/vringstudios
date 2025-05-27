11af638afd3b0bb790636bf9fe3a42624af2fd77
Rupam Das <rupam1624@gmail.com>
2025-05-28 03:28:38 +0530

    Update route.ts

--- src/app/api/getmessages/route.ts
+++ src/app/api/getmessages/route.ts
@@ -5,7 +5,7 @@ import UserModel from "@/model/User";
 import { User } from "next-auth";
 import mongoose from "mongoose";
 
-export async function GET(request: Request) {
+export async function GET(_request: Request) {
   await dbConnect();
   const session = await getServerSession(authOptions);
   const user: User = session?.user as User;
